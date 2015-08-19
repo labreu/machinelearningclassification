1e4d59d9e852aec5e28abfb094df833418431eb2
Lucas <lucasribeiroabreu@gmail.com>
2015-08-19 14:02:29 -0300

    http://sam-marsh.staff.shef.ac.uk/mas115/docs/PYTHON-Lecture10.pdf

--- mlclassification.py
+++ mlclassification.py
@@ -1,22 +1,37 @@
-#Differential equation
-
-
 import numpy as np
-from scipy import integrate
 import matplotlib.pyplot as plt
+from sklearn import svm
 
-def deriv(y,t):
-    yprime = np.array([3.5*y[0]])
-    return yprime
+x = [1, 5, 1.5, 8, 1, 9]
+y = [2, 8, 1.8, 8, 0.6, 11]
+plt.scatter(x,y)
+#plt.show()
+    
 
-start=0
-end=1
-numsteps=1000
-time=np.linspace(start,end,numsteps)
-y0=np.array([10])
+X = np.array([[1,2],
+             [5,8],
+             [1.5,1.8],
+             [8,8],
+             [1,0.6],
+             [9,11]])
+y = [0,1,0,1,0,1]
 
-y=integrate.odeint(deriv,y0,time)
-plt.plot(time,y[:])
+clf = svm.SVC(kernel='linear', C = 1.0)
+clf.fit(X,y)
+
+print(clf.predict([0.58, 0.76]))
+print(clf.predict([10.58, 10.76]))
+
+w = clf.coef_[0]
+print(w)
+
+a = -w[0] / w[1]
+
+xx = np.linspace(0, 12)
+yy = a * xx - clf.intercept_[0] / w[1]
+
+h0 = plt.plot(xx, yy, 'k-', label="non weighted div")
+
+plt.scatter(X[:, 0], X[:, 1])
+plt.legend()
 plt.show()
-
-
